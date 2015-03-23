60c06b0dff0ddbe5d119073f9e09825847ae8d66
k942 <k942@users.noreply.github.com>
2015-03-23 22:40:26 +0100

    test with jersey

--- src/test/java/io/gazelle/jerseytest/resources/IndexTest.java
+++ src/test/java/io/gazelle/jerseytest/resources/IndexTest.java
@@ -1,8 +1,16 @@
 package io.gazelle.jerseytest.resources;
 
+import io.gazelle.GazelleClient;
+import io.gazelle.resources.index.Index;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
@@ -11,23 +19,43 @@ import org.junit.Test;
 
 public class IndexTest extends JerseyTest {
 
-	@Path("hello")
+	@Path("ajax.php")
 	public static class HelloResource {
+
 		@GET
-		public String getHello() {
-			return "Hello World!";
+		public Response getHello(@QueryParam("action") String action) {
+			String s = null;
+			if (action.equals("index")) {
+				s = "{\n" + " \"status\": \"success\",\n" + " \"response\": {\n" + " \"username\": \"dr4g0n\",\n"
+						+ " \"id\": 469,\n" + " \"authkey\": \"redacted\",\n" + " \"passkey\": \"redacted\",\n"
+						+ " \"notifications\": {\n" + " \"messages\": 0,\n" + " \"notifications\": 9000,\n"
+						+ " \"newAnnouncement\": false,\n" + " \"newBlog\": false\n" + " },\n" + " \"userstats\": {\n"
+						+ " \"uploaded\": 585564424629,\n" + " \"downloaded\": 177461229738,\n" + " \"ratio\": 3.29,\n"
+						+ " \"requiredratio\": 0.6,\n" + " \"class\": \"VIP\"\n" + " }\n" + " }\n" + "}";
+			}
+			return Response.ok().type(MediaType.APPLICATION_JSON).entity(s).build();
+		}
+	}
+
+	@Path("login.php")
+	public static class LoginResource {
+
+		@POST
+		public Response post() {
+			return Response.ok().cookie(new NewCookie("session", "Hello, world!")).build();
 		}
 	}
 
 	@Override
 	protected Application configure() {
-		return new ResourceConfig(HelloResource.class);
+		return new ResourceConfig(HelloResource.class, LoginResource.class);
 	}
 
 	@Test
 	public void test() {
-		final String hello = target("hello").request().get(String.class);
-		Assert.assertEquals("Hello World!", hello);
+		GazelleClient client = GazelleClient.newInstance("http://localhost:9998", "blu", "lawl");
+		Index index = client.request("index", Index.class);
+		Assert.assertEquals("dr4g0n", index.getResponse().getUsername());
 	}
 
 }
